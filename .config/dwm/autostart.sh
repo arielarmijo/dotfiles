3df829b2d317118f5265759cf3978cdec3bed620
cochayuyo <tumorlike@gmail.com>
2019-06-23 16:04:30 -0400

    CPU usage as a function in bash script

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -2,6 +2,13 @@
 feh --bg-scale /home/aarmijo/.wallpapers/space_01.png 
 urxvtd -q -o -f
 compton -b 
+volumeicon &
+nm-applet & 
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/evolution-data-server/evolution-alarm-notify &
+/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh &
+export SSH_AUTH_SOCK
+
 #conky | while read LINE; do xsetroot -name "$LINE"; done &
 
 # Icons for indicators (otf-font-awesome)
@@ -20,77 +27,99 @@ date_format="%a %d %b %Y"
 time_format="%H:%M"
 refresh_time=5
 
-while true; do
+status=""
+tail=0
+new=(0 0 0 0 0 0 0 0 0 0)
 
-	tail=0 #number of spaces after the status line (needed for keep text position)
-	cpu_usage=$(cpu_usage)
-	
-	if [ $cpu_usage -ge $cpu_usage_max ]; then
-		status="\x03$cpu_icon $cpu_usage%\x01 "
+function cpu_usage {
+	prev=("${new[@]}")
+	new=($(sed -n 's/^cpu\s\s//p' /proc/stat))
+#	echo "      user nice system idle iowait  irq  softirq steal guest guest_nice"
+#	echo "prev: ${prev[@]}"
+#	echo "new:  ${new[@]}"
+	total_time=$(( (${new[0]} + ${new[1]} + ${new[2]} +${new[3]} + ${new[4]} + ${new[5]} + ${new[6]} + ${new[7]}) \
+	- \
+	(${prev[0]} + ${prev[1]} + ${prev[2]} +${prev[3]} + ${prev[4]} + ${prev[5]} + ${prev[6]} + ${prev[7]}) ))
+	total_idle=$(( (${new[3]}+${new[4]}) - (${prev[3]}+${prev[4]}) ))
+	total_usage=$(($total_time - $total_idle))
+	usage=$(($total_usage*100/$total_time))
+#	echo "$usage%"
+	if [ $usage -ge $cpu_usage_max ]; then
+		status+="\x03$cpu_icon $usage%\x01 "
 		tail=$(($tail+1))
 	else
-		status="$cpu_icon $cpu_usage% "
+		status+="$cpu_icon $usage% "
 	fi
 	
-	cpu_load_1m="$(cat /proc/loadavg | awk '{print $1}')"
-	cpu_load_5m="$(cat /proc/loadavg | awk '{print $2}')"
-	cpu_load_15m="$(cat /proc/loadavg | awk '{print $3}')"
+}
+
+function cpu_load {
+	load_1m="$(cat /proc/loadavg | awk '{print $1}')"
+	load_5m="$(cat /proc/loadavg | awk '{print $2}')"
+	load_15m="$(cat /proc/loadavg | awk '{print $3}')"
 	
-	if (($(echo "$cpu_load_1m > $cpu_load_max" |bc -l))) || \
-	   (($(echo "$cpu_load_5m > $cpu_load_max" |bc -l))) || \
-	   (($(echo "$cpu_load_15m > $cpu_load_max" |bc -l))); then
+	if (($(echo "$load_1m > $cpu_load_max" |bc -l))) || \
+	   (($(echo "$load_5m > $cpu_load_max" |bc -l))) || \
+	   (($(echo "$load_15m > $cpu_load_max" |bc -l))); then
 		status+="\x03$load_icon \x01"
 		tail=$(($tail+1))
 	else
 		status+="$load_icon "
 	fi
 	
-	if (( $(echo "$cpu_load_1m > $cpu_load_max" |bc -l) )); then
-		status+="\x03$cpu_load_1m\x01 "
+	if (( $(echo "$load_1m > $cpu_load_max" |bc -l) )); then
+		status+="\x03$load_1m\x01 "
 		tail=$(($tail+1))
 	else
-		status+="$cpu_load_1m "
+		status+="$load_1m "
 	fi
 
-	if (( $(echo "$cpu_load_5m > $cpu_load_max" |bc -l) )); then
-		status+="\x03$cpu_load_5m\x01 "
+	if (( $(echo "$load_5m > $cpu_load_max" |bc -l) )); then
+		status+="\x03$load_5m\x01 "
 		tail=$(($tail+1))
 	else
-		status+="$cpu_load_5m "
+		status+="$load_5m "
 	fi
 
-	if (( $(echo "$cpu_load_15m > $cpu_load_max" |bc -l) )); then
-		status+="\x03$cpu_load_15m\x01 "
+	if (( $(echo "$load_15m > $cpu_load_max" |bc -l) )); then
+		status+="\x03$load_15m\x01 "
 		tail=$(($tail+1))
 	else
-		status+="$cpu_load_15m "
+		status+="$load_15m "
 	fi
+}
 
-	cpu_temp=$(cat /sys/class/hwmon/hwmon1/temp1_input | cut -b 1-2)
-
-	if [ $cpu_temp -ge $cpu_temp_max ]; then
-		status+="\x03$temp_icon $cpu_temp°C\x01 $sep "
+function cpu_temp {
+	temp=$(cat /sys/class/hwmon/hwmon1/temp1_input | cut -b 1-2)
+	if [ $temp -ge $cpu_temp_max ]; then
+		status+="\x03$temp_icon $temp°C\x01 $sep "
 		tail=$(($tail+1))
 	else
-		status+="$temp_icon $cpu_temp°C $sep "
+		status+="$temp_icon $temp°C $sep "
 	fi
+}
 
+function date_time {
 	date=$(date +"$date_format")
 	time=$(date +"$time_format")
-
 	status+="$cal_icon $date $clock_icon $time"
+}
 
+function add_tail {
 	for (( c=1; c<=$tail; c++ )); do  
 		status+=" "
 	done
-	
-	xsetroot -name "$(echo -e "$status")"
-	sleep $(($refresh_time-1)) #cpu_usage takes 1 second to run
+}
 
+while true; do
+	status=""
+	tail=0
+	cpu_usage
+	cpu_load
+	cpu_temp
+	date_time
+	add_tail
+#	echo -e "$status"
+	xsetroot -name "$(echo -e "$status")"
+	sleep $refresh_time
 done &
-nm-applet & 
-volumeicon &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-/usr/lib/evolution-data-server/evolution-alarm-notify &
-eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
-export SSH_AUTH_SOCK
